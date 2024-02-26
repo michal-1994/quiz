815abaa8aa6a8bdbcde5d7f8d35be3597907e4f1
Michal <michalgrzegorczyk04@gmail.com>
2024-02-26 11:26:10 +0100

    detect quiz page and toggle progress bar

--- src/app/quiz/progress/progress.component.ts
+++ src/app/quiz/progress/progress.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { ActivatedRoute, Router, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs/operators';
+import { Subscription } from 'rxjs';
+import { CommonModule } from '@angular/common';
 
 @Component({
     selector: 'app-progress',
     standalone: true,
-    imports: [],
+    imports: [CommonModule],
     templateUrl: './progress.component.html',
     styleUrl: './progress.component.scss'
 })
-export class ProgressComponent {}
+export class ProgressComponent implements OnDestroy {
+    private routerSubscription: Subscription;
+    public currentUrl: string = '';
+
+    constructor(
+        private route: ActivatedRoute,
+        private router: Router
+    ) {
+        this.routerSubscription = this.router.events
+            .pipe(filter(event => event instanceof NavigationEnd))
+            .subscribe(() => {
+                this.currentUrl = this.router.url;
+            });
+    }
+
+    ngOnDestroy() {
+        if (this.routerSubscription) {
+            this.routerSubscription.unsubscribe();
+        }
+    }
+}
