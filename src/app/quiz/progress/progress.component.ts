f1197125c58224cc1ae3beb0bdc867520dc013cb
Michal <michalgrzegorczyk04@gmail.com>
2024-02-26 10:26:23 +0100

    add routing and quiz components

---
+++ src/app/quiz/progress/progress.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-progress',
+    standalone: true,
+    imports: [],
+    templateUrl: './progress.component.html',
+    styleUrl: './progress.component.scss'
+})
+export class ProgressComponent {}
