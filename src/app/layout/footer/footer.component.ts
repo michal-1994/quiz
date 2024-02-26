66ce189e53c5d7bf20aac60e7e46b8b6bdf1170d
Michal <michalgrzegorczyk04@gmail.com>
2024-02-26 10:21:18 +0100

    create footer copyrights

--- src/app/layout/footer/footer.component.ts
+++ src/app/layout/footer/footer.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
     selector: 'app-footer',
     standalone: true,
-    imports: [],
+    imports: [MatToolbarModule],
     templateUrl: './footer.component.html',
     styleUrl: './footer.component.scss'
 })
-export class FooterComponent {}
+export class FooterComponent {
+    getCopyrightInfo(): string {
+        const currentYear = new Date().getFullYear();
+        return `Copyright © ${currentYear} Quiz`;
+    }
+}
