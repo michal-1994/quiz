7c77f3a776d7b40b7d87ae28c224e62355b2e4be
Michal <michalgrzegorczyk04@gmail.com>
2024-02-26 10:14:35 +0100

    create header component

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
     selector: 'app-header',
     standalone: true,
-    imports: [],
+    imports: [MatToolbarModule, MatButtonModule, MatIconModule],
     templateUrl: './header.component.html',
     styleUrl: './header.component.scss'
 })
