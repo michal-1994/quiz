692eee04f78ee4f5c387c4df77505ecc97f97cbb
Michal <michalgrzegorczyk04@gmail.com>
2024-02-27 09:05:23 +0100

    add start quiz button and handle click event with redirect to quiz page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { Router } from '@angular/router';
 
 @Component({
     selector: 'app-home',
@@ -8,4 +9,11 @@ import { MatButtonModule } from '@angular/material/button';
     templateUrl: './home.component.html',
     styleUrl: './home.component.scss'
 })
-export class HomeComponent {}
+export class HomeComponent {
+    constructor(private router: Router) {}
+
+    handleStartQuiz(numberOfQuestions: number) {
+        console.log('handleStartQuiz: ', numberOfQuestions);
+        this.router.navigateByUrl('/quiz');
+    }
+}
